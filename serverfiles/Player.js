370d29e69afa2bbfc588c6d35885fdb671fdc448
Harsh Chittora <harshchittora2001@gmail.com>
2024-06-28 17:21:18 +0530

    player js file created

---
+++ serverfiles/Player.js
@@ -0,0 +1,12 @@
+class Player {
+    constructor(health, strength, attack) {
+      this.health = health;
+      this.strength = strength;
+      this.attack = attack;
+    }
+  
+    isAlive() {
+      return this.health > 0;
+    }
+  }
+  
